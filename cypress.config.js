8c4e543a81007beac3784fe2539bd27b436a1cdd
Bilal Karatas <bilalkaratash@gmail.comgit config --global user.name bilalkaratasgit config --global user.email bilalkaratash@gmail.com>
2023-09-23 17:16:17 -0400

    config updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // These settings apply everywhere unless overridden
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  video: false,
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
+  retries : 0,
+  
+  // Viewport settings overridden for component tests
+  component: {
+    viewportWidth: 500,
+    viewportHeight: 500,
+  },
   e2e: {
+    baseUrl: "https://...",
+    defaultCommandTimeout: 10000,       // Command timeout overridden for E2E tests
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
